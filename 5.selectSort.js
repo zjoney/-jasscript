3d8b7177bd76328482f69c06c121076b48c5900c
zjoney <zdjoey@126.com>
2022-07-29 15:19:51 +0800

    ⚡️ perf(排序) : 选择排序

--- 5.selectSort.js
+++ 5.selectSort.js
@@ -1,20 +1,21 @@
 
-// todo
+// 
 function selectSort(arr) {
   let len = arr.length;
   for (let i = 0; i < len - 1; i++) {
-    let minIndex = i;
-    for (let j = i; j < len; j++) {
+    let minIndex;
+    for (let j = i + 1; j < len; j++) {
       if (arr[minIndex] > arr[j]) {
         minIndex = j
       }
     }
+    temp = arr[i]
     // 最小值和第一个交换位置
-    [arr[j], arr[minIndex]] = [arr[minIndex], arr[j]]
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
   }
   return arr;
 }
 // test
 const arr = [3, 1, 2, 5, 4]
-console.log(bubbleSort(arr)) // [1, 2, 3, 4, 5]
-console.log(selectSort(arr)) // [1, 2, 3, 4, 5]
+console.log(selectSort2(arr)) // [1, 2, 3, 4, 5]
