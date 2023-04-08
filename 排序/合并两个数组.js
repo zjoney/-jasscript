3c106c1ad9471e4e3994f0417bf42745fdbccc21
zhangjing520 <zdjoey@126.com>
2023-04-08 10:15:37 +0800

    feat: 🎸 实现排序

--- 排序/合并两个数组.js
+++ 排序/合并两个数组.js
@@ -35,4 +35,7 @@ function merge(nums1, m, nums2, n) {
       nums1[tail] = temp;
       tail--;
   }
+  console.log(nums1)
 }
+const nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+merge(nums1, m, nums2, n)
